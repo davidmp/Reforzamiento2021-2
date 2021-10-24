90907ddfaa2bc7b4c4bd712ecf37453507120356
davidmp <mamanipari@gmail.com>
2021-10-24 21:46:28 +0000

    oper relac

--- ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
+++ ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
@@ -43,8 +43,14 @@ public class App{
         System.out.println(ologxx);
         int ologxxx=!(a>b || a==b )?1:0;
         System.out.println(ologxxx);
+        //operadores relacionales
+        System.out.println((a>b)?"verdadero":"falso");
+        System.out.println((a>=b)?"verdadero":"falso");
+        System.out.println((a<b)?"verdadero":"falso");
+        System.out.println((a<=b)?"verdadero":"falso");
+        System.out.println((a==b)?"verdadero":"falso");
+        System.out.println((a!=b)?"verdadero":"falso");
 
-        
         
     }
 
