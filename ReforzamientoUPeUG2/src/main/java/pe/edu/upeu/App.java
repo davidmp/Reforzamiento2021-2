eb56cf1a7557cba53e6a8e05628b138c5ebc4516
davidmp <mamanipari@gmail.com>
2021-10-24 20:32:13 +0000

    G2

---
+++ ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,41 @@
+package pe.edu.upeu;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+
+    public static void tiposdatos(){
+        byte a=1;
+        short b=8;
+        int c=87;
+        float d=84.5f;
+        double f=87.21;
+
+        boolean e=true; 
+
+        char h='+';
+
+        String g=" hola";
+        Integer xx;
+        Double xxx;
+        Date ss;
+        
+    }
+
+    public static void operadores() {
+        
+    }
+
+    public static void controlStructure() {
+        
+    }
+
+    public static void main( String[] args ){
+
+        System.out.println( "Hello World!" );
+    }
+}
