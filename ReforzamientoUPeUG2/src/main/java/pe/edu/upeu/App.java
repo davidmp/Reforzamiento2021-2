8579d523aebe1f2ed52ab3e85fb14bc689f370b9
davidmp <mamanipari@gmail.com>
2021-10-24 22:42:21 +0000

    ultimo ejercicio

--- ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
+++ ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
@@ -118,6 +118,17 @@ public class App{
     public static void main( String[] args ){
         //System.out.println( "Hello World!" );
         //operadores();
-        controlStructure();
+        //controlStructure();
+        tablaMultiplicar();
     }
+
+    public static void tablaMultiplicar() {
+        for (int i = 1; i<=10; i++) {
+            for (int j = 1; j <=10; j++) {
+                System.out.println(i+" x "+j+" = "+ (i*j));
+            }
+            System.out.println();
+        }
+    }
+
 }
